5949c6a7d713b1ee4d9129d693bf41519adb037c
NawazT <nawaztausif63@gmail.com>
2023-11-30 23:18:29 +0530

    day 29 approach 3

--- November Challenge/Day29-Numnber-of-1Bits.cpp
+++ November Challenge/Day29-Numnber-of-1Bits.cpp
@@ -36,3 +36,20 @@ public:
         return ans;
     }
 };
+
+// Approach 3
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        while(n > 0)
+        {
+            if(n%2 == 1) ans++;
+            n/=2;
+        }
+
+        return ans;
+    }
+};
+
